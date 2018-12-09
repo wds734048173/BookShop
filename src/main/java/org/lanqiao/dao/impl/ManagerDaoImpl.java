b44ae6722ee354c19e609a05a73b431a4c6c2bc7
rcm <lanqiao@lanqiao.org>
2018-12-09 22:34:29 +0800

    管理员信息代码提交

--- src/main/java/org/lanqiao/dao/impl/ManagerDaoImpl.java
+++ src/main/java/org/lanqiao/dao/impl/ManagerDaoImpl.java
@@ -3,19 +3,43 @@ package org.lanqiao.dao.impl;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.lanqiao.dao.IManagerDao;
+import org.lanqiao.domain.Condition;
 import org.lanqiao.domain.Manager;
 import org.lanqiao.utils.jdbcUtils;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManagerDaoImpl implements IManagerDao {
     private QueryRunner qr = new QueryRunner(jdbcUtils.getDataSource());
     @Override
-    public List<Manager> selectAll() throws SQLException {
-        String sql = "select * from tb_manager";
-        return qr.query(sql,new BeanListHandler<>(Manager.class));
+    public List<Manager> selectAll(Condition condition) throws SQLException {
+        StringBuffer sql = new StringBuffer("select * from tb_manager  where 1 = 1 ");
+        List<Object> search = new ArrayList<>();
+        if(condition.getName() != null && !"".equals(condition.getName())){
+            sql.append(" and AdminName like ? ");
+            search.add("%" + condition.getName() + "%");
+        }
+        List<Manager> managerList = new ArrayList<>();
+        try {
+            managerList = qr.query(sql.toString(),new BeanListHandler<>(Manager.class),search.toArray());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return managerList;
+    }
+
+    @Override
+    public void deleteManagerById(int AdminId) {
+        String sql = "DELETE FROM tb_manager WHERE AdminId = ?";
+        try {
+            qr.execute(sql,AdminId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -52,5 +76,22 @@ public class ManagerDaoImpl implements IManagerDao {
             e.printStackTrace();
         }
         return result;
+    }
+
+    @Override
+    public Long getManagerCount(Condition condition) {
+        StringBuffer sql = new StringBuffer("SELECT count(1) from tb_manager where 1 = 1 ");
+        List<Object> search = new ArrayList<>();
+        if(condition.getName() != null && !"".equals(condition.getName())){
+            sql.append(" and AdminName like ? ");
+            search.add("%" + condition.getName() + "%");
+        }
+        Long count = 0L;
+        try {
+            count = qr.query(sql.toString(),new ScalarHandler<>(1),search.toArray());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
     }
 }
